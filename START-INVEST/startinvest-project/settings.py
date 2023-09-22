afb06283373add1852fe1cedb081436407b090cd
Ravi-Shankar-K <127617287+Ravi-Shankar-K@users.noreply.github.com>
2023-09-22 12:40:18 +0530

    Update settings.py

--- START-INVEST/startinvest-project/settings.py
+++ START-INVEST/startinvest-project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xkb)-7rdeirjs0s4m3x@p*_+t$s(06w7@xi*irv!)!v11+@kip'
+SECRET_KEY = 'secret-key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -81,7 +81,7 @@ DATABASES = {
         'NAME' : 'startinvest',
         'HOST' : 'localhost',
         'USER' : 'root',
-        'PASSWORD' : 'sql_ravi',
+        'PASSWORD' : 'sql_password',
         'PORT' : '3306',
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
